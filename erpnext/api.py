444162b9ebf4c5142d488c3e3daed3c99f7ec1d3
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2022-07-19 19:33:09 +0700

    Update reset database scrip fix generate payment method

--- erpnext/api.py
+++ erpnext/api.py
@@ -850,7 +850,7 @@ def create_default_payment_type():
 
 
     # ABA Riel
-    if not frappe.db.exists("Mode of Payment",{"name":'ABA'}):
+    if not frappe.db.exists("Mode of Payment",{"name":'ABA KHR'}):
         doc= frappe.get_doc({
             "mode_of_payment": "ABA KHR",
             "enabled": 1,
